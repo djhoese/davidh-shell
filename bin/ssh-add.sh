ae7c2e0b76d6ebb79e596b15349834e24d5edf5d
David Hoese <david.hoese@ssec.wisc.edu>
2022-05-13 12:01:49 -0500

    Add various changes over the years

--- bin/ssh-add.sh
+++ bin/ssh-add.sh
@@ -7,6 +7,7 @@ try=0
 while [ ! -S "${PAM_KWALLET5_LOGIN}" ]; do
         [ $try -ge 10 ] && exit
         sleep 1
+        echo "Waiting for ssh-add.sh to find PAM_KWALLET5_LOGIN"
         try=$(($try+1))
 done
 
